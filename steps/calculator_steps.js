07edde8b3cb0d0e9aba905371c61cd70d8dbaa0f
Pinkie Aruwani <aruwanip@gmail.com>
2020-02-21 20:11:40 -0500

    Update step definition and Readme

--- steps/calculator_steps.js
+++ steps/calculator_steps.js
@@ -9,36 +9,30 @@ let CalculatorSteps = function() {
   };
 
   this.Given('The calculator is open', function () {
-    // Handiling asynchronous step definition with Promise return
     return this.page.get();
   });
 
-  this.When('I calculate $first $operator $second', function (first, operator, second, callback) {
-    // Handling asynchronous step definition with callback function
+  this.When('I calculate $first $operator $second', function (first, operator, second) {
     this.page.setLeftOperand(first);
     this.page.setOperator(operator);
     this.page.setRightOperand(second);
-    this.page.clickGo();
-    callback();
+    return this.page.clickGo();
   });
 
-  this.When('I enter first value of $first', function (first, callback) {
-    this.page.setLeftOperand(first);
-    callback();
+  this.When('I enter first value of $first', function (first) {
+    return this.page.setLeftOperand(first);
   });
 
-  this.When('I enter second value of $second', function (second, callback) {
-    this.page.setRightOperand(second);
-    callback();
+  this.When('I enter second value of $second', function (second) {
+    return this.page.setRightOperand(second);
   });
 
-  this.When('I click Go', function (callback) {
-    this.page.clickGo();
-    callback();
+  this.When('I click Go', function () {
+    return this.page.clickGo();
   });
 
-  this.Then('the result should equal $result', function (result, callback) {
-    expect(this.page.getResult()).to.eventually.equal(result).and.notify(callback);
+  this.Then('the result should equal $result', function (result) {
+    return expect(this.page.getResult()).to.eventually.equal(result);
   });
 };
 
